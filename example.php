1275ab127750f96aca6f20c97da1e9ab3f9d2f2a
nfell2009 <nfell2009@live.co.uk>
2016-05-27 00:24:29 +0100

    Update example.php

--- example.php
+++ example.php
@@ -6,3 +6,8 @@ foreach(getResources() as $name => $id) {
 	$resource = getResource($name, true); // true = full resource info includes; description, content, lastest version info and author
 	echo $resource->ResourceAuthor->username . "<br>";
 }
+
+echo "Here are the resources for nfell2009:<br>";
+foreach(resourcesOfAuthor() as $resource) {
+	echo $resource->name . "<br>";
+}
